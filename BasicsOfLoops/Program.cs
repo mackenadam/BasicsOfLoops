45121e1ab8b5f47ff9c3c8706ea8dec100944042
Adam Macken <mackenadam@gmail.com>
2021-03-05 16:29:03 +1300

    Basic do while loop

--- BasicsOfLoops/Program.cs
+++ BasicsOfLoops/Program.cs
@@ -8,6 +8,7 @@ namespace BasicsOfLoops
         {
             // Introduction to loops in C#
             // For loops
+            Console.WriteLine("For loop example:");
             for (int i = 10; i >= 0; i--)
             {
                 if (i > 0)
@@ -20,7 +21,23 @@ namespace BasicsOfLoops
                 }
             }
 
+            Console.WriteLine("...");
+
             // Do while loops
+            Console.WriteLine("Do while loop example:");
+            int lengthOfText = 0;
+            string wholeText = "";
+            do
+            {
+                Console.WriteLine("Please enter the name of a friend:");
+                string currentName = Console.ReadLine();
+                int currentLength = currentName.Length;
+                lengthOfText += currentLength;
+                wholeText += currentName + " ";
+            } while (lengthOfText < 20);
+            Console.WriteLine("Thanks, that was enough names: {0}", wholeText);
+
+            Console.WriteLine("...");
         }
     }
 }

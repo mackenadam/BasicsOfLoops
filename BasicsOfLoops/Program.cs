ce849207f815aab5d04061ec2e4be2264c834da0
Adam Macken <mackenadam@gmail.com>
2021-03-05 16:00:31 +1300

    Add project files.

---
+++ BasicsOfLoops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicsOfLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Introduction to loops in C#
+        }
+    }
+}
